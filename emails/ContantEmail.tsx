90feeca309430d718a28b6b95199b5551a6da63b
Stepan Krousky <stepan.krousky@seznam.cz>
2024-05-02 00:58:49 +0200

    Resend fix

--- emails/ContantEmail.tsx
+++ emails/ContantEmail.tsx
@@ -27,16 +27,14 @@ export default function ContactFormEmail({
       <Tailwind>
         <Head />
         <Fragment>
-          <Body className={`bg-gray-100 font-sans text-black`}>
+          <Body className="bg-gray-100 font-sans text-black">
             <Container>
               <Section className="my-10 rounded-lg border border-black bg-white px-10 py-4">
-                <Heading className="text-center text-lg leading-tight sm:text-3xl">
+                <Heading className="text-center text-lg leading-tight">
                   You received the following message from the contact form
                 </Heading>
                 <Hr />
-                <Text className="bg-white text-xl text-black sm:text-lg">
-                  {message}
-                </Text>
+                <Text className="bg-white text-xl text-black">{message}</Text>
                 <Text className="mt-5 text-base">
                   The sender&apos;s email is: {senderEmail}
                 </Text>
