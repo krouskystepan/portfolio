1c8e944c717c7ed6cc277c233e5e31756bd30e86
Štěpán Krouský <stepan.krousky@seznam.cz>
2025-10-22 23:12:50 +0200

    fix static clock

--- app/c/page.tsx
+++ app/c/page.tsx
@@ -1,6 +1,8 @@
 import { getServerTime } from '@/actions/getServerTime.action'
 import Countdown from '@/components/Countdown'
 
+export const dynamic = 'force-dynamic'
+
 export default async function CountdownPage() {
   const serverNow = await getServerTime()
   const targetDate = new Date('2025-10-31T22:00:00Z')
