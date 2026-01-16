57d4e92ca58b3f9ff0d695daf99ea4220800dd29
Štěpán Krouský <stepan.krousky@seznam.cz>
2026-01-16 22:54:56 +0100

    fix images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
+    unoptimized: true,
     qualities: [80, 90]
   }
   // Debugging purposes
